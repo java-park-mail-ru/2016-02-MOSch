1b1155e637225a12155f1913a5af7b01c649f3b6
Alexey Mokeev <olerdrive@gmail.com>
2016-06-03 01:26:57 +0300

    test2

--- src/main/java/db/datasets/UserDataSetDAO.java
+++ src/main/java/db/datasets/UserDataSetDAO.java
@@ -1,5 +1,7 @@
 package db.datasets;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
@@ -17,6 +19,8 @@ import java.util.List;
  */
 public class UserDataSetDAO {
     private final Session session;
+    private static final Logger LOGGER = LogManager.getLogger(UserDataSetDAO.class);
+
 
     public UserDataSetDAO(Session session) {
         this.session = session;
@@ -76,7 +80,7 @@ public class UserDataSetDAO {
         }
         if (multiplier != null) {
             oldDataSet.setPoints(oldDataSet.getPoints() + multiplier * newScore);
-            System.out.append(oldDataSet.getPoints().toString());
+            LOGGER.info(oldDataSet.getPoints());
         }
         if (!oldDataSet.getAccuracyBf() && dataSet.getAccuracyBf() && oldDataSet.getPoints() > 350000L) {
             oldDataSet.setPoints(oldDataSet.getPoints() - 350000L);
